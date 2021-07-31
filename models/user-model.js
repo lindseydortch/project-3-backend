316edcbbc2219bd8ff071edc6da2443786aaa1ca
lindseydortch <codewithlinds@gmail.com>
2021-07-31 15:50:13 -0500

    updated schemas

--- models/user-model.js
+++ models/user-model.js
@@ -8,6 +8,18 @@ const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  // groups: [{
+  //   // References use the type ObjectId
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   // the name of the model to which they refer
+  //   ref: 'Group'
+  // }],
+  // events: [{
+  //   // References use the type ObjectId
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   // the name of the model to which they refer
+  //   ref: 'Event'
+  // }]
 });
 
 const User = mongoose.model("User", UserSchema);
